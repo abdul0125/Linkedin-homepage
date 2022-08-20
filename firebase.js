696523402688832afbd65a7231a03c58bb901b1c
Abdul Ahad <abdul0125@gmail.com>
2022-08-20 17:47:35 +0530

    new commit

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCakL2dgJ12nbK71m3bHT8gTKBc7_Hpldg",
+  authDomain: "linkedin-clone-aaf9b.firebaseapp.com",
+  projectId: "linkedin-clone-aaf9b",
+  storageBucket: "linkedin-clone-aaf9b.appspot.com",
+  messagingSenderId: "153611790925",
+  appId: "1:153611790925:web:a9774487df73f623034a04"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
